20d6754285d4525b1812d5656bdb4b5203491a6b
Anders Sundin <olsu2201@student.miun.se>
2024-02-08 17:47:47 +0100

    skapar grundstruktur och väljer färger

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  
+  // Hantera mobil navigation
+isMobileNavigationVisible: boolean = false;
+
+toggleMobileNavigation(): void {
+  this.isMobileNavigationVisible = !this.isMobileNavigationVisible;
+}
+
 
 }
